d192e146fb701fae5b33c0be6332cbde565eb4ec
wisdom <bsbpsj97@naver.com>
2023-02-03 00:06:35 +0900

    board test

--- src/main/java/com/test/project/repository/TbUserRepository.java
+++ src/main/java/com/test/project/repository/TbUserRepository.java
@@ -3,5 +3,5 @@ package com.test.project.repository;
 import com.test.project.entity.TbUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TbUserRepository extends JpaRepository<TbUser, Long> {
+public interface TbUserRepository extends JpaRepository<TbUser,Long> {
 }
